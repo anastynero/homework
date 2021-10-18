bd196ac0b01dec8bc1390d1762f7f2bb4c683024
anastynero <90865165+anastynero@users.noreply.github.com>
2021-10-18 20:50:52 +0300

    Add files via upload

---
+++ homework2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму: ");
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num > 500)
+            {
+                Console.WriteLine(num -((num * 3) / 100));
+            }
+            else if (num>1000)
+            {
+                Console.WriteLine(num - ((num * 5) / 100));
+            }
+
+        }
+    }
+}
