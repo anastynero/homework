d74afce2c9078b65a0a08679473b2930ce12b628
anastynero <90865165+anastynero@users.noreply.github.com>
2021-11-17 23:08:27 +0300

    Add files via upload

---
+++ num4.cs
@@ -0,0 +1,26 @@
+﻿// Вывести на экран, консольного приложения, прямоугольный треугольник из звездочек “*”, с длиной катета m.
+using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= s; i++)
+            {
+                int num = 0;
+                while (num < i)
+                {
+                    Console.Write("*");
+                    num++;
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    }
+}
