3f48268d666e6b23dd72614fae7299bb6573123e
anastynero <90865165+anastynero@users.noreply.github.com>
2021-11-17 23:02:56 +0300

    Add files via upload

---
+++ num3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= s; i++)
+            {
+                int num = 0;
+                while (num < i)
+                {
+                    Console.Write("*");
+                    num++;
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
+    }
+}
