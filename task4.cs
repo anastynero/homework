9da7c0d9700455f0c489a2bc7c8f5eda77b543a2
anastynero <90865165+anastynero@users.noreply.github.com>
2021-11-19 18:04:06 +0300

    Add files via upload

---
+++ task4.cs
@@ -0,0 +1,20 @@
+﻿//Найти сумму первых N членов арифметической прогрессии с использованием цикла for
+using System;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i <= num1; i++)
+            {
+                sum += i;  
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
