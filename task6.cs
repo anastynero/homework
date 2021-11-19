e4545cf24b7fa53bc63080df92ece7908cd3dcb0
anastynero <90865165+anastynero@users.noreply.github.com>
2021-11-19 18:31:23 +0300

    Add files via upload

---
+++ task6.cs
@@ -0,0 +1,20 @@
+﻿//Вычислить факториал числа p(при p > 0), используя цикл do while.
+using System;
+
+namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = Convert.ToInt32(Console.ReadLine());
+            int i = 1;
+            do
+            {
+                i*= p;
+                p-=1;
+            } while (p>1);
+            Console.WriteLine(i);
+        }
+    }
+}
