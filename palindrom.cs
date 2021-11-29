676344b3dbd3a7e5eebdc7ececbd870b2ad45381
anastynero <90865165+anastynero@users.noreply.github.com>
2021-11-29 09:49:19 +0300

    Add files via upload

---
+++ palindrom.cs
@@ -0,0 +1,36 @@
+﻿// Проверить является введенный пользователем палиндромом или нет. Пробелы и регистр символов в строке игнорировать.
+using System;
+
+namespace palindrom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string s1 = s.Replace(" ", "");
+            Console.WriteLine($"{s1}");
+            bool tr = true;
+            for (int i = 0; i < s1.Length; i++)
+            {
+                if (s1[i] == s1[s1.Length - 1 - i])
+                {
+                    tr = true;
+                }
+                else
+                {
+                    tr = false;
+                }
+                if (!tr)
+                {
+                    Console.WriteLine("Это не палиндром");
+                    break;
+                }
+            }
+            if (tr)
+            {
+                Console.WriteLine("Это палиндром");
+            }
+        }
+    }
+}
