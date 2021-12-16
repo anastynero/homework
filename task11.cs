0869726037fdf1866ec4bdabde596bbff0bb3161
anastynero <90865165+anastynero@users.noreply.github.com>
2021-12-16 18:21:38 +0300

    Add files via upload

---
+++ task11.cs
@@ -0,0 +1,34 @@
+﻿/*
+            Задана последовательность целых чисел.
+            Подсчитать количество элементов, у которых четные соседи.
+*/
+using System;
+
+namespace task11
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = Array.ConvertAll(Console.ReadLine().Split(), input => int.Parse(input));
+
+            Console.WriteLine(Sosedi(nums));
+        }
+
+        private static int Sosedi(int[] array)
+        {
+            int count = 0;
+
+            if (array[1] % 2 == 0)
+            { count++; }
+            if (array[array.Length - 2] % 2 == 0)
+            { count++; }
+
+            for (int i = 1; i < array.Length - 1; i++)
+                if (array[i - 1] % 2 == 0 && array[i + 1] % 2 == 0)
+                { count++; }
+
+            return count;
+        }
+    }
+}
