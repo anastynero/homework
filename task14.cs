8b3b4602f42a0a5dedc1b4f7d0e3146fb500b12d
anastynero <90865165+anastynero@users.noreply.github.com>
2021-12-17 11:42:32 +0300

    Add files via upload

---
+++ task14.cs
@@ -0,0 +1,28 @@
+﻿//Выполнить: Вводятся три числа — длины трех сторон треугольника. Создайте функцию Perimeter(), которая вычисляет периметр треугольника по длинам трех его сторон.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static int Perimeter(int a, int b, int c)
+        {
+            int result = a + b + c;
+            return result;
+        }
+    static void Main(string[] args)
+    {
+        int a, b, c, res;
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        c = int.Parse(Console.ReadLine());
+        res = Perimeter(a, b, c);
+        Console.WriteLine(res);
+    }
+}
+}
