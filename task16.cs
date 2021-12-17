3d8469d75a36ee45856d25c021fdeee490f66ceb
anastynero <90865165+anastynero@users.noreply.github.com>
2021-12-17 15:51:21 +0300

    Add files via upload

---
+++ task16.cs
@@ -0,0 +1,28 @@
+﻿//Найти все целые числа из промежутка от 1 до 300, у которых ровно пять делителей.
+using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 2; i < 300; i++)
+            {
+                int k = 0;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        k++;
+                    }
+                    if (k == 5)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
