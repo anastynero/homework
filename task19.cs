0e911e1c4cdab1d5b68fd9f58188f81625ef1919
anastynero <90865165+anastynero@users.noreply.github.com>
2021-12-17 20:34:10 +0300

    Add files via upload

---
+++ task19.cs
@@ -0,0 +1,22 @@
+﻿//В одномерном массиве из 100 чисел M[] подсчитайте количество элементов, удовлетворяющих условию 0 < M[i] <125
+using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];
+            Random rand = new Random();
+            for (int x = 0; x < array.Length; x++)
+            {
+                int y = rand.Next(250);
+                if ((y > 0) & (y < 125))
+                {
+                    Console.WriteLine($"{y}");
+                }
+            }
+        }
+    }
+}
