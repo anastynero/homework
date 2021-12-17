fc7c8869e51bdcafcba9ba7a458fbd88c3e0b7e6
anastynero <90865165+anastynero@users.noreply.github.com>
2021-12-17 20:31:36 +0300

    Add files via upload

---
+++ task18.cs
@@ -0,0 +1,20 @@
+﻿//Написать программу для получения реверсированной строки.
+using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Convert.ToString(Console.ReadLine());
+            string revers = "";
+            for (int i = 0; i < (str.Length); i++)
+            {
+                revers += str[(str.Length) - 1 - i];
+
+            }
+            Console.WriteLine(revers);
+        }
+    }
+}
