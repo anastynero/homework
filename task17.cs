e6ec39a5a6235942f7e5e909e0caedaec5d251c0
anastynero <90865165+anastynero@users.noreply.github.com>
2021-12-17 20:28:12 +0300

    Add files via upload

---
+++ task17.cs
@@ -0,0 +1,29 @@
+﻿//Написать программу, программу, которая осуществляет капитализацию введенного пользователем текста. В качестве разделителя слов использовать пробел
+using System;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            string str = Convert.ToString(Console.ReadLine());
+            string probel = "";
+            for (int i = 0; i < (str.Length); i++)
+            {
+                if (i != 0)
+                {
+                    probel += str[i];
+                }
+                else
+                {
+                    probel += str[i];
+                    probel = probel.ToUpper();
+                }
+
+            }
+            Console.WriteLine($"{probel}");
+        }
+    }
+}
